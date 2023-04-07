fa145b50a96ad2dd430f85e6a6279b9bc1600f99
Athrun <erythron@qq.com>
2023-04-07 14:57:31 +0800

    add ssh key

--- init server/init.sh
+++ init server/init.sh
@@ -2,7 +2,7 @@
  # @Author: Athrun
  # @Email: erythron@outlook.com
  # @Date: 2022-12-10 10:22:22
- # @LastEditTime: 2023-03-27 10:29:23
+ # @LastEditTime: 2023-04-07 14:54:32
  # @description: now it is null
 ###
 
@@ -19,8 +19,10 @@ resultUlimit=`echo $1 | grep 'ulimit'`
 resultUmask=`echo $1 | grep 'umask'`
 resultCron=`echo $1 | grep 'cron'`
 resultHostKey=`echo $1 | grep 'hostkey'`
+resultSshPK=`echo $1 | grep 'sshpk'`
 user=$2
 home=$3
+
 
 function sudoIssue {
   content=`sudo cat /etc/sudoers| grep "# Defaults secure_path"`
@@ -42,7 +44,7 @@ function selinuxOff {
 function iptablesOff {
     sudo systemctl stop firewalld.service
     sudo systemctl disable firewalld.service
-    sudo iptables -F
+    #sudo iptables -F
 }
 
 function sshdConfig {
@@ -95,6 +97,16 @@ EOF
   fi
 }
 
+function sshPublicKey {
+  sudo mkdir $home/.ssh
+  cat << EOF | sudo tee $home/.ssh/authorized_keys
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC55q6wQT/YovW4fRQ1+pajQC7/ocntYmOG6yMFjjH0glxxwV4UplnVEVNyaeIBX+J4CaaU2dzQ84+2Utx2Qtt5qH1Wj/pJAL10+uQqfuZed3ZhmQWdnzxUVVQeWHNTb4kP7I2EDusYiY0afd/HDQSi5GQLbIrEFtGSZqRnKVJ9V2Bs6+6tHNtoQHzbTRrCMoSKbFCwJaZ5JPRpzN81zG4KRsxayBRnJUQQRwx65yszj7/sIV4L2XB98/lLicLwjr1gVfg+0L7k9rvjzWFAN/teSxqC/ZkwDTr/jCC1q8m9LvVietHCTiC7A0mH0F77Xe2rsr46Kg2TjuiOC9mnR80NZFvU5l9UZkpPZMxDIyKi9VxjTS4PSbFe+/bteXxh5EAkg+YiHMY5Xlr9njF38aJPBdKzvTOm3OFmPofusWdCtMyISTNQ3DrhwMafloho0vk9ImdNitLDXqjt+AyY9I0E36FUe+vq9Yj57DkOdhSlhp0GdTP0pGQzD+Eg1JYDjB8= ctgcloud@telcom.cn
+EOF
+  sudo chmod 600 $home/.ssh/authorized_keys
+  sudo chown $user:$user $home/.ssh/authorized_keys
+}
+
+
 if [[ -n $resultSudo ]] ;then
     echo 'info: exec sudoIssue'
     sudoIssue
@@ -128,6 +140,11 @@ fi
 if [[ -n $resultHostKey ]] ;then
     echo 'info: exec hostKeyConfig'
     hostKeyConfig
+fi
+
+if [[ -n $resultSshPK ]] ;then
+    echo 'info: exec sshPublicKey'
+    sshPublicKey
 fi
 
 selinuxOff
